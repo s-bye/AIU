2dd172f7e2a367a7f0e17b3bdedb350f7e84aebb
Abay <kill46923@gmail.com>
2023-11-16 00:14:49 +0600

    Copy of timestamps deleted

--- Snakify/Input, print and numbers/Two_timestamps — копия.py
+++
@@ -1,9 +0,0 @@
-stud_a = int(input())
-stud_b = int(input())
-stud_c = int(input())
-
-desks_a = (stud_a + 1) // 2
-desks_b = (stud_b + 1) // 2
-desks_c = (stud_c + 1) // 2
-
-print(desks_a + desks_b + desks_c)
