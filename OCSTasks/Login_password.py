60345f1d84feb9f535ffb17d558d499330997102
Abay <kill46923@gmail.com>
2023-11-19 15:45:26 +0600

    Modified Login_password.py

--- OCSTasks/Login_password.py
+++ OCSTasks/Login_password.py
@@ -1,4 +1,26 @@
-login = input()
-password = input()
-true_login = '230121033'
-true_password = '12345678'
+import time
+while True:
+    login = input()
+    if login.isdigit():
+        break
+    else:
+        print("Invalid username")
+while True:
+    password = input()
+    if password.isdigit():
+        break
+    else:
+        print("Invalid password")
+attempts = 0
+while True:
+    true_login = '230121033'
+    true_password = '12345678'
+    if login == true_login and password == true_password:
+        print('Access granted')
+        break
+    else:
+        attempts += 1 
+        if attempts => 3:
+            print('Too many failed attempts, please try again after 5 seconds')
+            time.sleep(5)
+            attempts = 0
