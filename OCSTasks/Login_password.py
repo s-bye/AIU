e3d8d978f817a8e73d90b9d0732f4c9c92ac7251
s-bye <125466455+s-bye@users.noreply.github.com>
2023-11-20 12:23:40 +0600

    Update Login_password.py

--- OCSTasks/Login_password.py
+++ OCSTasks/Login_password.py
@@ -1,27 +1,32 @@
-# Make it by functions and fix the attempts bug
 import time
-while True:
+def data_input():
     login = input()
-    if login.isdigit():
-        break
-    else:
-        print("Invalid username")
-while True:
     password = input()
-    if password.isdigit():
-        break
-    else:
-        print("Invalid password")
-attempts = 0
-while True:
+    return login, password
+
+def validating_data(login, password):
     true_login = '230121033'
     true_password = '12345678'
     if login == true_login and password == true_password:
+        return True
+    else:
+        return False
+
+attempts = 0
+attempts_left = 3
+
+while True:
+    login, password = data_input()
+    if validating_data(login, password):
         print('Access granted')
         break
     else:
-        attempts += 1 
-        if attempts == 3:
-            print('Too many failed attempts, please try again after 5 seconds')
-            time.sleep(5)                   
-            attempts = 0            
+        attempts += 1
+        attempts_left -= 1  
+        print(f'Invalid login or password, please recheck your login and password and try again. Attempts left: {attempts_left}')
+    if attempts == 3:
+        print('Too many incorrect attempts. Please try again after 5 seconds')
+        time.sleep(5)
+        print('Now you can try again!')
+        attempts = 0
+        attempts_left = 3
