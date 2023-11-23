4b9e142ad3341108dba2229dea13f82936d312b3
Abay <kill46923@gmail.com>
2023-11-23 19:35:42 +0600

    Delete: Deleted Helping_martians

--- OCSTasks/Helping_martians.py
+++
@@ -1,8 +0,0 @@
-import random 
-boxes = [231, 235, 247]
-distance = 7 
-def generate_legs(distances):
-    legs = [False] * (distance + 1)
-    for distance in distances:
-        legs[distance] = True 
-    return legs
