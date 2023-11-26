c9bf9092298248afaf0eae6f118877bf48a8eadb
Abay <kill46923@gmail.com>
2023-11-26 23:50:44 +0600

    Modify: Caps was fixed, added entering kilometer marks from user

--- Projects/Helping the Martians/Helping_martians.py
+++ Projects/Helping the Martians/Helping_martians.py
@@ -1,5 +1,13 @@
 import random 
-CARGO_WEIGHT = 713
-NUM_BOXES = 3
-BOX_LOCATIONS = [0, 0, 0]
-CARGO_FOUND = False
+cargo_weight = 713
+num_boxes = 3
+box_locations = [0, 0, 0]
+cargo_found = False
+
+while not cargo_found:
+    kilometer_marks = []
+    for i in range(num_boxes):
+        for i in range(num_boxes):
+            kilometer_mark = int(input(f'Enter the kilometer mark for box {i + 1}: '))
+            kilometer_marks.append(kilometer_mark)
+            
