1c5c080fea6bc2d4cb5975db3ae45581aa40576e
Abay <kill46923@gmail.com>
2023-11-29 19:08:44 +0600

    Modify: Code is divided into def functions

--- Projects/Helping the Martians/Helping_martians.py
+++ Projects/Helping the Martians/Helping_martians.py
@@ -1,23 +1,44 @@
 import random
 
-cargo_weight = [300, 250, 163]
+def generate_cargo_location():
+    return random.sample(range(1, 8), 3)
 
-while True:
-    cargo_location = random.sample(range(1, 8), 3)
-    print(f'Current cargo location: {cargo_location}')
-    kilometer_marks = []
-    for i in range(3):
-        kilometer_mark = int(input(f'Enter the kilometer mark for box {i + 1}: '))
-        kilometer_marks.append(kilometer_mark)
-    total_weight = 0
-    cargo_found = True
+def calculate_total_weight(cargo_weight, kilometer_marks, cargo_location):
+    total_weight = sum(cargo_weight[i] for i in range(3) if kilometer_marks[i] == cargo_location[i])
+    return total_weight
+
+def check_cargo_location(kilometer_marks, cargo_location, cargo_weight):
     for i in range(3):
         if kilometer_marks[i] != cargo_location[i]:
-            cargo_found = False
+            return False
+    
+    total_weight = 0
+    for i in range(3):
+        if kilometer_marks[i] == cargo_location[i]:
+            total_weight += cargo_weight[i]
+    
+    return total_weight == 713
+
+def main():
+    cargo_weight = [300, 250, 163]
+
+    while True:
+        cargo_location = generate_cargo_location()
+        print(f'Current cargo location: {cargo_location}')
+        kilometer_marks = []
+        
+        for i in range(3):
+            kilometer_mark = int(input(f'Enter the kilometer mark for box {i + 1}: '))
+            kilometer_marks.append(kilometer_mark)
+        
+        total_weight = calculate_total_weight(cargo_weight, kilometer_marks, cargo_location)
+        cargo_found = check_cargo_location(kilometer_marks, cargo_location, cargo_weight)
+        
+        if cargo_found and total_weight == 713:
+            print('Yes')
             break
-        total_weight += cargo_weight[i]
-    if cargo_found and total_weight == 713:
-        print('Yes')
-        break
-    else:
-        print('No')
+        else:
+            print('No')
+
+if __name__ == "__main__":
+    main()
